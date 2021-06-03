26fd265322254dfa0b9b146b917c2aa7be08d6ad
Yihoo <yihoozero@gmail.com>
2021-06-03 21:38:49 +0800

    add charpter01 content

---
+++ charpter01/02.js
@@ -0,0 +1,16 @@
+var CheckObject = function () {
+  return {
+    checkName: function () {
+    //  验证姓名
+    },
+    checkEmail: function () {
+    //  验证邮箱
+    },
+    checkPassword: function () {
+    //  验证密码
+    }
+  }
+};
+
+var a = CheckObject();
+a.checkName();
