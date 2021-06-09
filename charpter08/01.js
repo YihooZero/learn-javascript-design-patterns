f8e6f8a1055e0e270b91f6ede8d8c99372da762f
Yihoo <yihoo1991@outlook.com>
2021-06-09 17:26:42 +0800

    add charpter08 content

---
+++ charpter08/01.js
@@ -0,0 +1,25 @@
+var A = {
+  Util: {
+    util_method1: function() {},
+    util_method2: function() {},
+    // ...
+  },
+  Tool: {
+    tool_method1: function() {},
+    tool_method2: function() {},
+    // ...
+  },
+  Ajax: {
+    get: function() {},
+    post: function() {},
+    // ...
+  },
+  others: {
+    // ...
+  },
+  // ...
+};
+
+A.Util.util_method1();
+A.Tool.tool_method1();
+A.Ajax.get();
