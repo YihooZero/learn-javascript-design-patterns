faca8aa903c1e16c1b1c3065a93748cff06b9775
Yihoo <yihoo1991@outlook.com>
2021-06-10 17:55:48 +0800

    add charpter14 content

---
+++ charpter14/01.js
@@ -0,0 +1,17 @@
+var News = function () {
+  // 子组件容器
+  this.children = [];
+  // 当前组件元素
+  this.element = null;
+}
+News.prototype = {
+  init: function () {
+    throw new Error('请重写你的方法');
+  },
+  add: function () {
+    throw new Error('请重写你的方法');
+  },
+  getElement: function () {
+    throw new Error('请重写你的方法');
+  },
+}
