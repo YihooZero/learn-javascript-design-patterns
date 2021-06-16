354c1dc38766d4169289b03234356e9934807a55
Yihoo <yihoo1991@outlook.com>
2021-06-16 08:34:15 +0800

    modify charpter19 01.js

--- charpter20/01.js
+++ charpter20/01.js
@@ -90,3 +90,22 @@ var createValidateResult = function (data, dom) {
   // 显示校验结果
   dom.parentNode.getElementsByTagName('span')[0].innerHTML = data;
 }
+
+dealData('用户名不正确', 'validate', input[0]);
+dealData(123, 'sug', input[1]);
+dealData(['爱奇艺', '阿里巴巴', '爱漫画'], 'sug', input[1]);
+dealData({
+  'iqy': '爱奇艺',
+  'albb': '阿里巴巴',
+  'imh': '爱漫画'
+}, 'sug', input[1]);
+
+var input = document.getElementsByTagName('input');
+// 监听内容改变事件做内容校验
+input[0].onchange = function (e) {
+  sendData({ value: input[0].value }, 'validate', input[0]);
+}
+// 监听键盘事件对内容做提示处理
+input[1].onkeydown = function (e) {
+  sendData({ value: input[1].value }, 'sug', input[1]);
+}
