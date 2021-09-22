a2879416181e3050edad6e07f0ff6ebf30244254
Yihoo <yihoo1991@outlook.com>
2021-09-22 14:49:57 +0800

    modify charpter18 01&02.js

--- charpter18/01.js
+++ charpter18/01.js
@@ -1,68 +1,34 @@
-// 创建超级玛丽状态类
-var MarryState = function () {
-  // 内部状态私有变量
-  var _currentState = {},
-    // 动作与状态方法映射
-    states = {
-      jump: function () {
-        // 跳跃
-        console.log('jump');
-      },
-      move: function () {
-        // 移动
-        console.log('move');
-      },
-      shoot: function () {
-        // 射击
-        console.log('shoot');
-      },
-      squat: function () {
-        // 蹲下
-        console.log('squat');
-      }
-    };
-  // 动作控制类
-  var Action = {
-    // 改变状态方法
-    changeState: function () {
-      // 组合动作通过传递多个参数实现
-      var arg = arguments;
-      // 重置内部状态
-      _currentState = {};
-      // 如果有动作则添加动作
-      if (arg.length) {
-        // 遍历动作
-        for (var i = 0, len = arg.length; i < len; i++) {
-          // 向内部状态中添加动作
-          _currentState[arg[i]] = true;
-        }
-      }
-      // 返回动作控制类
-      return this;
+// 投票结果状态对象
+var ResultState = function () {
+  // 判断结果保存在内部状态中
+  var States = {
+    // 每种状态作为一种独立方法保存
+    state0: function () {
+      // 处理结果0
+      console.log('这是第一种情况')
     },
-    // 执行动作
-    goes: function () {
-      console.log('触发一次动作');
-      // 遍历内部状态保存的动作
-      for (var i in _currentState) {
-        // 如果该动作存在则执行
-        states[i] && states[i]();
-      }
-      return this;
+    state1: function () {
+      // 处理结果1
+      console.log('这是第二种情况')
+    },
+    state2: function () {
+      // 处理结果2
+      console.log('这是第三种情况')
+    },
+    state3: function () {
+      // 处理结果3
+      console.log('这是第四种情况')
     }
+  };
+  // 获取某一种状态并执行其对应的方法
+  function show(result) {
+    States['state' + result] && States['state' + result]();
   }
-  // 返回接口方法change、goes
   return {
-    change: Action.changeState,
-    goes: Action.goes
+    // 返回调用状态方法接口
+    show: show
   }
-}
+}();
 
-// 创建一个超级玛丽
-var marry = new MarryState();
-marry
-  .change('jump', 'shoot')
-  .goes()
-  .goes()
-  .change('shoot')
-  .goes();
+// 展示结果3
+ResultState.show(3);
